75a26779458f7872a4aa44c9783ec921be947666
Milton Junior <miltonjr27@gmail.com>
2012-07-16 19:24:25 -0300

    Another changes...

---
+++ app/controllers/admin/categorias_controller.rb
@@ -0,0 +1,9 @@
+class Admin::CategoriasController < Admin::CrudController
+
+protected
+
+  def record_type
+    Categoria
+  end
+
+end
---
+++ app/models/categoria.rb
@@ -0,0 +1,7 @@
+class Categoria < ActiveRecord::Base
+
+  validates :nome, :presence => true, :uniqueness => true
+
+  attr_accessible :nome
+
+end
