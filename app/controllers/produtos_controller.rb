76005727e689b46eeb27485049f7fdea40629bb0
Milton Junior <miltonjr27@gmail.com>
2012-07-16 19:48:37 -0300

    With Heroku

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -17,16 +17,5 @@ class ProdutosController < ApplicationController
   def new
     @produto = Produto.new
   end
-
-  def create
-    @produto = Produto.new(params[:produto])
-    if @produto.save
-      flash[:success] = 'O produto foi salvo com sucesso'
-      redirect_to produtos_path
-    else
-      flash.now[:error] = 'Ocorreram erros na hora de gravar o produto'
-      render :new
-    end
-      
-    end
+  
 end
